f2f7ec56805e5a1d0235b8fbec3870e32f801e70
Herley <herleyoliveira19@gmail.com>
2023-04-11 21:57:21 -0300

    feat: data storage in device

--- App.tsx
+++ App.tsx
@@ -1,20 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+    useEffect,
+    useState,
+} from 'react';
+import {
+    Button,
+    Text,
+    TextInput,
+    View,
+} from 'react-native';
+import {
+    MMKV,
+    useMMKVString,
+} from 'react-native-mmkv';
 
+import {
+    styles,
+} from './styles';
+
+// Criação de instância
+const storage = new MMKV({ id: "datastorage" });
+
+type User = {
+  name: string;
+  email: string;
+};
 export default function App() {
+  const [name, setName] = useMMKVString("user.name");
+  const [email, setEmail] = useMMKVString("user.email");
+
+  const [user, setUser] = useState<User>();
+
+  // Salvar informações
+  const handleSave = () => {
+    storage.set("user", JSON.stringify({ name, email }, null, 2));
+  };
+
+  // Buscar as informações salva no dispositivo
+  const fetchUser = () => {
+    const data = storage.getString("user");
+    setUser(data ? JSON.parse(data) : {});
+  };
+
+  // listener para observar o que alterou
+  useEffect(() => {
+    const listner = storage.addOnValueChangedListener((changedKey) => {
+      const newValue = storage.getString("user");
+
+      console.log("Chave ->", changedKey);
+      console.log("Novo valor ->", newValue);
+    });
+
+    return () => listner.remove();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput placeholder="Nome..." style={styles.input} onChangeText={setName} value={name} />
+      <TextInput
+        placeholder="E-mail..."
+        style={styles.input}
+        onChangeText={setEmail}
+        value={email}
+      />
+
+      <Button title="Salvar" onPress={handleSave} />
+
+      <Text style={styles.text}>
+        {user?.name} - {user?.email}
+      </Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
